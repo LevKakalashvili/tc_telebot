9a6f3d8b21b6fcea566959af541c5aaae4dcb19a
Lev <kakalashvililev@yandex.ru>
2023-01-13 17:46:10 +0300

    feat: added sending screenshot to chat

--- app_bot/run.py
+++ app_bot/run.py
@@ -2,21 +2,31 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 from app_bot.config import Config as AppConfig
+from app_bot.utils import is_valid_url, get_screenshot
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
 bot = Bot(token=AppConfig.BOT_TOKEN)
 dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.answer("Введите url.")
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
 
-    # TODO: url validation
+    if is_valid_url(message.text):
+        file = get_screenshot(message.text)
+        if file is not None:
+            await message.answer_document(open(file, mode="rb"))
+            # await message.answer(f'Правильный url: {message.text}')
 
-    await message.answer(message.text)
+    else:
+        await message.answer(f'Не верный url: {message.text}')
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
