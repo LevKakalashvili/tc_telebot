ec2a876057f5c2c156914377425b90d755439d8c
Lev <kakalashvililev@yandex.ru>
2023-01-14 19:14:51 +0300

    chore: some changes

--- app_bot/config.py
+++ app_bot/config.py
@@ -1,11 +1,12 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    SCREENSHOT_FOLDER = os.getenv("SCREENSHOT_FOLDER")
-    SCREENSHOT_RESOLUTION_WIDTH = int(os.getenv("SCREENSHOT_RESOLUTION_WIDTH"))
-    SCREENSHOT_RESOLUTION_HEIGHT = int(os.getenv("SCREENSHOT_RESOLUTION_HEIGHT"))
+    SCREENSHOT_FOLDER = str(os.getenv("SCREENSHOT_FOLDER"))
+    SCREENSHOT_RESOLUTION_WIDTH = int(str(os.getenv("SCREENSHOT_RESOLUTION_WIDTH")))
+    SCREENSHOT_RESOLUTION_HEIGHT = int(str(os.getenv("SCREENSHOT_RESOLUTION_HEIGHT")))
