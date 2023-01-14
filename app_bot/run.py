576a681bc08b196d2e005c2f134e972140e0d254
Lev <kakalashvililev@yandex.ru>
2023-01-14 14:31:18 +0300

    refactor: changed logic

--- app_bot/run.py
+++ app_bot/run.py
@@ -2,7 +2,7 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 from app_bot.config import Config as AppConfig
-from app_bot.utils import is_valid_url, get_screenshot
+from app_bot.utils import screenshot_maker as scm
 
 logging.basicConfig(level=logging.INFO)
 
@@ -10,23 +10,19 @@ bot = Bot(token=AppConfig.BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=["start", "help"])
 async def send_welcome(message: types.Message):
     await message.answer("Введите url.")
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-
-    if is_valid_url(message.text):
-        file = get_screenshot(message.text)
-        if file is not None:
-            await message.answer_document(open(file, mode="rb"))
-            # await message.answer(f'Правильный url: {message.text}')
-
+    if scm.make_sreeenshot(url=message.text):
+        await message.answer_document(open(scm.file, mode="rb"))
+        await message.answer(f"Код ответа: {scm.status_code}")
     else:
-        await message.answer(f'Не верный url: {message.text}')
+        await message.answer(f"{scm.message}:\n{message.text}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
