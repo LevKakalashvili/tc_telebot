3fa16001f20a08205b8d098f833041fc6a274fd4
Lev <kakalashvililev@yandex.ru>
2023-01-15 14:52:45 +0300

    refactor: refactor

--- app_bot/config.py
+++ app_bot/config.py
@@ -7,9 +7,10 @@ load_dotenv()
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    SCREENSHOT_FOLDER = \
-        str(os.getenv("SCREENSHOT_FOLDER")) \
-        if str(os.getenv("SCREENSHOT_FOLDER")) \
+    SCREENSHOT_FOLDER = (
+        str(os.getenv("SCREENSHOT_FOLDER"))
+        if str(os.getenv("SCREENSHOT_FOLDER"))
         else os.path.join(os.getcwd(), "screenshots_default")
+    )
     SCREENSHOT_RESOLUTION_WIDTH = int(str(os.getenv("SCREENSHOT_RESOLUTION_WIDTH")))
     SCREENSHOT_RESOLUTION_HEIGHT = int(str(os.getenv("SCREENSHOT_RESOLUTION_HEIGHT")))
