1c583137b71069ff754e08c7c63cb0ba642425d3
Lev <Kakalashvilillev@yandex.ru>
2023-01-16 19:37:13 +0300

    chore: changed default folder detection

--- app_bot/config.py
+++ app_bot/config.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+scr_folder = os.getenv("SCREENSHOT_FOLDER")
+
+if not scr_folder or scr_folder is None:
+    scr_folder = os.path.join(os.getcwd(), "screenshots_default")
+
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    SCREENSHOT_FOLDER = (
-        str(os.getenv("SCREENSHOT_FOLDER"))
-        if str(os.getenv("SCREENSHOT_FOLDER"))
-        else os.path.join(os.getcwd(), "screenshots_default")
-    )
+    SCREENSHOT_FOLDER = scr_folder
     SCREENSHOT_RESOLUTION_WIDTH = int(str(os.getenv("SCREENSHOT_RESOLUTION_WIDTH")))
     SCREENSHOT_RESOLUTION_HEIGHT = int(str(os.getenv("SCREENSHOT_RESOLUTION_HEIGHT")))
